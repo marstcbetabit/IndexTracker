66787c30b1c6d2bc7936f7a77943a81bd4bb097b
Thijs Cornelis Marschalk <t.marschalk@betabit.nl>
2025-05-22 13:19:05 +0200

    Refactor: use extension methods for service registration and DB migration, clean up Program.cs

--- Program.cs
+++ Program.cs
@@ -7,31 +7,53 @@ using IndexTracker.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace IndexTracker
 {
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIndexTrackerServices(this IServiceCollection services)
+        {
+            services.AddDbContext<IndexDbContext>(options =>
+                options.UseSqlite("Data Source=indextracker.db"));
+            services.AddScoped<IIndexValueRepository, IndexValueRepository>();
+            services.AddScoped<ISp500Service, Sp500Service>();
+            services.AddHostedService<IndexValueBackgroundService>();
+            return services;
+        }
+    }
+
+    public static class HostExtensions
+    {
+        public static void EnsureDatabaseMigrated(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<IndexDbContext>();
+            db.Database.Migrate();
+        }
+    }
+
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
+                })
                 .ConfigureServices((_, services) =>
                 {
-                    services.AddDbContext<IndexDbContext>(options =>
-                        options.UseSqlite("Data Source=indextracker.db"));
-                    services.AddScoped<IIndexValueRepository, IndexValueRepository>();
-                    services.AddScoped<ISp500Service, Sp500Service>();
-                    services.AddHostedService<IndexValueBackgroundService>();
+                    services.AddIndexTrackerServices();
                 });
 
             var app = builder.Build();
 
             // Ensure database and migrations are applied
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<IndexDbContext>();
-                db.Database.Migrate();
-            }
+            app.EnsureDatabaseMigrated();
 
             await app.RunAsync();
         }
